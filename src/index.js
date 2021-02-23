6287e4e855e4d1c50709653beae8a42ed819bd54
Dhirendra-byte <68893214+Dhirendra-byte@users.noreply.github.com>
2021-02-23 15:50:01 +0530

    nav and screen created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import Controller from "./Controller";
-
+import "./index.css";
+import "./style/main.scss";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
