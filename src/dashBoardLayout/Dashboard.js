5fbb6c98baba33de3cdb06529c90eace15813987
Dhirendra-byte <68893214+Dhirendra-byte@users.noreply.github.com>
2021-02-23 15:50:24 +0530

    ok

--- src/dashBoardLayout/Dashboard.js
+++ src/dashBoardLayout/Dashboard.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import "./_dashboardLayout.scss"
 
 const DashboardLayout=({children})=>{
   return(
-    <div>
-      {children}
+    <div className="dashboard-layout">
+      <nav className="side-nav-bar">
+         this is nav
+      </nav>
+      <div className="main-container">
+        here are children
+        {children}
+      </div>
     </div>
   )
 }
